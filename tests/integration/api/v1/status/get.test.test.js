c60da412657ed3a215159ec0bd4c930c7858b020
leonardo schwuedller <77402907+leoschwedler@users.noreply.github.com>
2026-08-20 06:56:00 -0300

    feat: add enpoint `api/v1/status`

---
+++ tests/integration/api/v1/status/get.test.test.js
@@ -0,0 +1,6 @@
+const url = "http://localhost:3000";
+
+test("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch(url + "/api/v1/status");
+  expect(response.status).toBe(200);
+});
